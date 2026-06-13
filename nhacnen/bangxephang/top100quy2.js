f459ec8a9e0c3fbae90b3b7e69d166fac3aa0aab
nhipdaptraitim <cutyyeunhac@gmail.com>
2026-06-14 07:35:44 +0900

    Add files via upload

--- nhacnen/bangxephang/top100quy2.js
+++ nhacnen/bangxephang/top100quy2.js
@@ -1,1 +1,30 @@
+// bangxephang/top100-quy2.js
 
+window.artistPlaylist = [
+
+    
+{title:"Sao Em Vẫn Yêu", 
+artist:"Thu Thuỷ",
+src:"mp3/Sao Em Van Yeu - Thu Thuy.mp3",
+cover:"img/thu-thuy.jpg"},
+
+{title:"Dằm Trong Tim",
+artist:"Lương Bích Hữu",
+src:"mp3/Dằm Trong Tim - Lương Bích Hữu.mp3",
+cover:"img/Lương Bích Hữu.jpg"},
+
+{title:"Biết Tìm Đâu",
+artist:"Tuấn Hưng",
+src:"mp3/Biet Tim Dau - Tuan Hung.mp3",
+cover:"img/tuấn hưng.jpg"},
+
+{title:"Tình Em Đại Dương",
+artist:"Duy Mạnh",
+src:"mp3/tinh em đại dương.mp3",
+cover:"img/duy mánh.jpg"},
+
+{title:"Lợi Danh",
+artist:"Phạm Khánh Hưng",
+src:"mp3/lợi danh.mp3",
+cover:"img/phamkhanhhung.jpg"},
+];
